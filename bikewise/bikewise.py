4bad8c31bbb3ea31b9713c5f101cf0f376fa5685
Ira Horecka <ira89@icloud.com>
2020-03-29 16:36:18 -0700

    initial commit - create framework for library

---
+++ bikewise/bikewise.py
@@ -0,0 +1,16 @@
+import json
+import requests
+
+def api_method(method):
+    def wrapper(*args, **kwargs):
+        return
+
+    return wrapper
+
+
+class BaseAPI():
+    pass
+
+
+class BikeWise():
+    pass
